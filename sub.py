467568e70a3db5d3edea12366671f92d334fff38
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2022-03-10 14:35:06 +0900

    意味のない変更

--- sub.py
+++ sub.py
@@ -3,3 +3,27 @@
 ゲームとは何にも関係ない
 
 """
+import tkinter 
+root = tkinter.Tk()
+root.geometry("300x200")
+root.title("hello world")
+frame = tkinter.Frame(root)
+frame.pack()
+
+
+
+label = tkinter.Label(frame,
+    text="""
+    こんにちは
+    こんにちは
+    こんにちは
+    こんにちは
+    こんにちは
+    """,
+    width=30, 
+    anchor=tkinter.W, 
+    justify='left'
+)
+label.pack()
+
+root.mainloop()
